9958304fc5bca7a5140fc40cf0aa79e5a91b9c18
陈鹏 <simple19930611@gmail.com>
2020-06-12 15:48:22 +0800

    init

---
+++ logview-base/src/main/java/me/simple/logview/ILogView.kt
@@ -0,0 +1,14 @@
+package me.simple.logview
+
+interface ILogView {
+
+    fun v(tag: String, msg: String)
+
+    fun d(tag: String, msg: String)
+
+    fun i(tag: String, msg: String)
+
+    fun w(tag: String, msg: String)
+
+    fun e(tag: String, msg: String)
+}
---
+++ logview/src/main/java/me/simple/logview/LogView.kt
@@ -0,0 +1,21 @@
+package me.simple.logview
+
+
+object LogView : ILogView {
+
+    override fun v(tag: String, msg: String) {
+    }
+
+    override fun d(tag: String, msg: String) {
+    }
+
+    override fun i(tag: String, msg: String) {
+    }
+
+    override fun w(tag: String, msg: String) {
+    }
+
+    override fun e(tag: String, msg: String) {
+    }
+
+}
