ea8f261e9af10832c710d63bc2aa1b9bb5658eb7
陈鹏 <simple19930611@gmail.com>
2020-06-15 15:58:04 +0800

    update:WindowLayout

--- logview/src/main/java/me/simple/logview/WindowLayout.kt
+++ logview/src/main/java/me/simple/logview/WindowLayout.kt
@@ -4,26 +4,42 @@ import android.content.Context
 import android.graphics.PixelFormat
 import android.os.Build
 import android.view.Gravity
+import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
 import android.widget.FrameLayout
 import android.widget.ImageView
+import java.util.Collections.max
 
 
 class WindowLayout(context: Context) : FrameLayout(context) {
 
-    private val mWindowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-    private val mLayoutParams = WindowManager.LayoutParams()
-    private val mIvMenu: ImageView by lazy { findViewById<ImageView>(R.id.ivMenu) }
+    private val mWM = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    private val mParams = WindowManager.LayoutParams()
+    private val mIvMenu by lazy { findViewById<ImageView>(R.id.ivMenu) }
+    private val mViewContent by lazy { findViewById<View>(R.id.viewContent) }
 
     init {
         View.inflate(context, R.layout.layout_log_view, this)
+        mIvMenu.setOnClickListener {
+            clickMenu()
+        }
+    }
+
+    private fun clickMenu() {
+        if (mViewContent.visibility == View.GONE) {
+            mViewContent.visibility = View.VISIBLE
+            mWM.updateViewLayout(this, matchLayoutParams())
+        } else {
+            mViewContent.visibility = View.GONE
+            mWM.updateViewLayout(this, wrapLayoutParams())
+        }
     }
 
     fun show() {
         if (isShown) return
         try {
-            mWindowManager.addView(this, wrapLayoutParams())
+            mWM.addView(this, wrapLayoutParams())
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -31,38 +47,61 @@ class WindowLayout(context: Context) : FrameLayout(context) {
 
     fun dismiss() {
         try {
-
+            mWM.removeView(this)
         } catch (e: Exception) {
             e.printStackTrace()
         }
     }
 
     private fun wrapLayoutParams(): WindowManager.LayoutParams {
-        mLayoutParams.width = LayoutParams.WRAP_CONTENT
-        mLayoutParams.height = LayoutParams.WRAP_CONTENT
+        mParams.width = LayoutParams.WRAP_CONTENT
+        mParams.height = LayoutParams.WRAP_CONTENT
         basicLayoutParams()
-        return mLayoutParams
+        return mParams
     }
 
     private fun matchLayoutParams(): WindowManager.LayoutParams {
-        mLayoutParams.width = LayoutParams.MATCH_PARENT
-        mLayoutParams.height = Utils.getScreenHeight() / 2
+        mParams.width = LayoutParams.MATCH_PARENT
+        mParams.height = Utils.getScreenHeight() / 2
         basicLayoutParams()
-        return mLayoutParams
+        return mParams
     }
 
     private fun basicLayoutParams() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-            mLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
+            mParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
         } else {
-            mLayoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+            mParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
         }
-        mLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-        mLayoutParams.format = PixelFormat.TRANSLUCENT
-        mLayoutParams.gravity = Gravity.TOP or Gravity.LEFT
+        mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+        mParams.format = PixelFormat.TRANSLUCENT
+        mParams.gravity = Gravity.TOP or Gravity.LEFT
         //        mLayoutParams.x = 10
         //        mLayoutParams.y = attr.y
     }
 
+    private var downX = 0f
+    private var downY = 0f
+    private var lastY = 0f
 
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+//        super.onTouchEvent(event)
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                downX = event.rawX
+                downY = event.rawY
+                lastY = downY
+            }
+            MotionEvent.ACTION_MOVE -> {
+                mParams.y += (event.rawY - lastY).toInt()
+                mParams.y = kotlin.math.max(0, mParams.y)
+                mWM.updateViewLayout(this, mParams)
+                lastY = event.rawY
+            }
+            MotionEvent.ACTION_UP -> {
+
+            }
+        }
+        return true
+    }
 }
